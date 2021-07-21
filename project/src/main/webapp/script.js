888f22f7cff07fb46ecb59ed4d6bb46f3af0fa92
lavanv1107 <vatanaklavan@gmail.com>
2021-07-21 15:02:13 +0000

    Add new function for footer.

--- project/src/main/webapp/script.js
+++ project/src/main/webapp/script.js
@@ -12,32 +12,48 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/*** Landing Page ***/
-/* Enter workout into the selected workout line by pressing on button */
+/*** Workout Selection ***/
+// Enter a workout into the selected workout line by pressing on a button 
 function clickWorkout(x) {
     document.getElementById("workout-select").value = x;
 }
 
-/*** Playlist Page ***/
-
-
 /*** Side Navigation Menu ***/
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// Set the width of the side navigation to 250px and the left margin of the page content to 250px 
 function openMenu() {
     document.getElementById("side-menu").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+// Set the width of the side navigation to 0 and the left margin of the page content to 0 
 function closeMenu() {
     document.getElementById("side-menu").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
 
-/* Activate the dark-mode class in the style sheet on the body */
+// Activate the dark-mode class in the style sheet on the body 
 function darkMode() {
     const x = document.body;
     x.classList.toggle("dark-mode");
 }
 
+/*** Footer ***/
+// Get the button
+mybutton = document.getElementById("scroll-top-button");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
